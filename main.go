e55fc57dae39141a167610d25f50ccdf225683f0
fachrunwira <fachrunwirap@gmail.com>
2025-05-20 14:02:12 +0700

    update to add logger

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/fachrunwira/basic-template-go-echo/middlewares/ratelimiting"
 	"github.com/fachrunwira/basic-template-go-echo/routes"
+	"github.com/natefinch/lumberjack"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
@@ -13,6 +14,18 @@ import (
 )
 
 func main() {
+	// Logger using lumberjack
+	logFile := &lumberjack.Logger{
+		Filename:   "./log/app.log",
+		MaxSize:    10,   // Max file size before rotation
+		MaxBackups: 3,    // Max old log files to retain
+		MaxAge:     28,   // Max days to retain a log files
+		Compress:   true, // Compress old files
+	}
+
+	log.SetOutput(logFile)
+
+	// Load .env files
 	errEnv := godotenv.Load()
 	if errEnv != nil {
 		log.Fatalf("Failed to open env files: %v", errEnv)
@@ -22,6 +35,13 @@ func main() {
 	appPort := os.Getenv("APP_PORT")
 
 	e := echo.New()
+
+	e.Logger.SetOutput(logFile)
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Output: logFile,
+		Format: `${time_rfc3339} | ${status} | ${method} | ${uri} | ${latency_human}` + "\n",
+	}))
 
 	// Whitelist IP
 	// allowedIPs := []string{}
